ad9293ccce53e1026cd82876c0c34f9bea0cc6e2
Maksym <maksick2005@gmail.com>
2023-04-08 18:49:56 +0300

    not all done

--- Lab1/src/work/src/main/java/ua/lviv/iot/TandemBicycle.java
+++ Lab1/src/work/src/main/java/ua/lviv/iot/TandemBicycle.java
@@ -1,4 +1,4 @@
-package ua.lviv.iot.algo.part1.lab1;
+package work.src.main.java.ua.lviv.iot;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
---
+++ Lab1/src/work/src/test/java/ua/lviv/iot/BicycleTest.java
@@ -0,0 +1,24 @@
+package work.src.test.java.ua.lviv.iot;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BicycleTest {
+
+    @Test
+    void getMaxDistance() {
+    }
+
+    @Test
+    void accelerate() {
+    }
+
+    @Test
+    void brake() {
+    }
+
+    @Test
+    void slowDown() {
+    }
+}
